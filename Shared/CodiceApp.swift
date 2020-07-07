69a211dcc678dff0141246c82dd89f5c17b5e0e0
Aymane Bengrina <bengrina.aymane@gmail.com>
2020-07-07 09:44:35 +0200

    Initial Commit

---
+++ Shared/CodiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodiceApp.swift
+//  Shared
+//
+//  Created by Aymane on 07/07/2020.
+//
+
+import SwiftUI
+
+@main
+struct CodiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
